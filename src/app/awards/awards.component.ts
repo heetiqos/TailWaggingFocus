eb083f7291fdcd1d25e311e0a4ed283e0f7925b5
Krsitine Ortega <masokrive_ort@yahoo.com>
2024-04-15 19:56:44 +0200

    with the new version from Maria's code

--- src/app/awards/awards.component.ts
+++ src/app/awards/awards.component.ts
@@ -8,10 +8,8 @@ import { Component } from '@angular/core';
 export class AwardsComponent {
   mouseDelta: number = 0;
   mouseDownPosition: number | null = null;
-  maxDelta = window.innerWidth / 2;
-
+   maxDelta = window.innerWidth / 2;
   constructor() { }
-
   onMouseDown(event: MouseEvent | TouchEvent) {
     if ('clientX' in event) {
       this.mouseDownPosition = (event as MouseEvent).clientX;
@@ -20,37 +18,25 @@ export class AwardsComponent {
     }
     console.log('Mouse down at:', this.mouseDownPosition);
   }
-
   onMouseMove(event: MouseEvent | TouchEvent) {
     if (this.mouseDownPosition !== null) {
       const clientX = ('clientX' in event) ? (event as MouseEvent).clientX : (event as TouchEvent).touches[0].clientX;
       this.mouseDelta = this.mouseDownPosition - clientX;
       const percentage = (this.mouseDelta / this.maxDelta) * 100;
       console.log('Mouse moved to:', this.mouseDelta);
-      
-      if (this.mouseDownPosition !== null) {
-        const track = document.getElementById("image-track");
-        if (!track) return;
-        
-        if (!track.dataset) return; 
-        
-        const mouseDelta = parseFloat(this.mouseDownPosition.toString()) - clientX;
-        const maxDelta = window.innerWidth / 2;
-        
-        const nextPercentageUnconstrained = parseFloat(track.dataset['prevPercentage'] || "0") + (mouseDelta / maxDelta) * -100;
-        const nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
-        
-        track.style.transform = `translate(${nextPercentage}%, -50%)`;
-        track.dataset['percentage'] = nextPercentage.toString();
-        
-        const images = Array.from(track.getElementsByClassName("image")) as HTMLElement[];
-        for (const image of images) {
-          image.style.objectPosition = `${100 + nextPercentage}% center`;
-        }
+      const track = document.getElementById("image-track");
+      if (!track) return;
+      if (!track.dataset) return;
+      const nextPercentageUnconstrained = parseFloat(track.dataset['prevPercentage'] || "0") + (this.mouseDelta / this.maxDelta) * -100;
+      const nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+      track.style.transform = `translateX(${nextPercentage}%)`;
+      track.dataset['percentage'] = nextPercentage.toString();
+      const images = Array.from(track.getElementsByClassName("image")) as HTMLElement[];
+      for (const image of images) {
+        image.style.objectPosition = `${100 + nextPercentage}% center`;
       }
     }
   }
-
   onMouseUp() {
     this.mouseDelta = 0;
     this.mouseDownPosition = null;
